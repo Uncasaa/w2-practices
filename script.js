ad75c2b3fab0bfbcb06f70288d1e568bf779f4a3
uncasa <uncasaa@gmail.com>
2021-10-26 15:04:34 +0200

    loadevent fastLog

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
+
+
+function toggleMenu(event) {
+    // console.log(event.target.classList);
+    // console.log(event.target);
+    // event.target.classList.toggle("clicked");
+    const randomText = "ezt vajon kilogolja e ?"
+    const fastLogResult = fastLog("Ezt a szöveget szeretném kilogoltatni");
+    fastLog(randomText);
+    console.log(fastLogResult);
+    document.getElementById("about").classList.add("clickedToo");
+};
+
 function loadEvent() {
-    console.log('the page has loaded');
+    // console.log('the page has loaded');
+    document.getElementById("menu-btn").addEventListener("click", toggleMenu);
+};
 
-    document.getElementById("menu-btn").addEventListener("click", function (){
-        console.log(event.target);
-        event.target.classList.toggle("clicked");
-        
-    });
-
-}
+function fastLog(text){
+    console.log(text);
+    return "muhaha";
+};
 
 window.addEventListener("load", loadEvent);
+
